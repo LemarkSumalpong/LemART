b4a7cfb0397d810b3006d91f714e6dd5ac64d5f4
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-01-29 00:17:08 +0800

    added scrollbar and logo

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,8 @@
+/**
+ * @copyright 2025 Lemark Sumalpong
+ * @license Apache-2.0
+ */
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+import tailwindscrollbar from 'tailwind-scrollbar';
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'san': ['Inter', 'sans-serif']
+      }
+    },
   },
-  plugins: [],
+  plugins: [tailwindscrollbar],
 }

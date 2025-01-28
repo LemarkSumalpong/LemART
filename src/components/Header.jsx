898ebe1e6b4320272808230459c4e8797b4603dc
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-01-29 00:40:25 +0800

    added header and logo

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+/**
+ * @copyright 2025 Lemark Sumalpong
+ * @license Apache-2.0
+ */
+
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="fixed top-0 left-0 w-full h-20 flex
+    items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
+      <div className="max-w-screen-2xl w-full mx-auto px-4">
+        <h1>
+          <a href="/"
+          className="logo"
+          >
+            <img src="images/logo.svg"
+            width={40}
+            height={40}
+            alt="Lemark" />
+
+          </a>
+        </h1>
+
+      </div>
+
+    </header>
+  )
+}
+
+export default Header

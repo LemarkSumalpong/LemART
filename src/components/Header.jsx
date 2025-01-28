4ccbb58355063b7d47e2b3c37615221c8deba133
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-01-29 01:20:38 +0800

    added navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,17 @@
  * @license Apache-2.0
  */
 
-import React from 'react'
+import Navbar from "./Navbar"
+
+
 
 const Header = () => {
   return (
     <header className="fixed top-0 left-0 w-full h-20 flex
     items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
-      <div className="max-w-screen-2xl w-full mx-auto px-4">
+      <div className="max-w-screen-2xl w-full mx-auto px-4
+      flex justify-between items-center md:px-6 md:grid 
+      md:grid-cols-[1fr,3fr,1fr]">
         <h1>
           <a href="/"
           className="logo"
@@ -17,10 +21,31 @@ const Header = () => {
             <img src="images/logo.svg"
             width={40}
             height={40}
-            alt="Lemark" />
+            alt="Lemark"
+            />
 
           </a>
         </h1>
+
+        <div className="relative md:justify-self-center">
+            <button
+             className="menu-btn md:hidden"
+             onClick={null}
+            >
+                <span className="material-symbols-rounded">
+                   menu
+                </span> 
+            </button>
+
+            <Navbar />
+        </div>
+
+        <a
+         href="#contact"
+         className=""
+        >
+         Contact Me  
+        </a>
 
       </div>
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+/**
+ * @copyright 2025 Lemark Sumalpong
+ * @license Apache-2.0
+ */
+
+const Navbar = () => {
+  return (
+    <div>
+      Navbar
+    </div>
+  )
+}
+
+export default Navbar

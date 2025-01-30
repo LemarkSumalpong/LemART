b0a4823f5d20cfe8d98a34587780c1ca9683249c
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-01-30 23:59:55 +0800

    for skill section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,9 +3,68 @@
  * @license Apache-2.0
  */
 
+const skillItem = [
+    {
+      imgSrc: '/images/figma.svg',
+      label: 'Figma',
+      desc: 'Design tool'
+    },
+    {
+      imgSrc: '/images/css3.svg',
+      label: 'CSS',
+      desc: 'User Interface'
+    },
+    {
+      imgSrc: '/images/javascript.svg',
+      label: 'JavaScript',
+      desc: 'Interaction'
+    },
+    {
+      imgSrc: '/images/nodejs.svg',
+      label: 'NodeJS',
+      desc: 'Web Server'
+    },
+    {
+      imgSrc: '/images/expressjs.svg',
+      label: 'ExpressJS',
+      desc: 'Node Framework'
+    },
+    {
+      imgSrc: '/images/mongodb.svg',
+      label: 'MongoDB',
+      desc: 'Database'
+    },
+    {
+      imgSrc: '/images/react.svg',
+      label: 'React',
+      desc: 'Framework'
+    },
+    {
+      imgSrc: '/images/tailwindcss.svg',
+      label: 'TailwindCSS',
+      desc: 'User Interface'
+    },
+  ];
+
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <section className="section">
+        <div className="container">
+            <h2 className="headline-2">
+                Tools I used
+            </h2>
+            <p className="">
+            Discover the powerful tools and technologies I use to create exceptional, high-performing websites & applications.
+            </p>
+            <div className="">
+                {
+                    skillItem.map(({imgSrc, label, desc}, key))
+                }
+            </div>
+        </div>
+
+
+    </section>
   )
 }
 

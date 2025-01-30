c3bcb9c7d8d20eec7fd25dd52c29ccf0ac22f4ea
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-01-30 23:53:55 +0800

    added components of skills in the main

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Skills from "./components/Skills";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
     <main>
       <Hero/>
       <About />
+      <Skills />
     </main>
     
     </>

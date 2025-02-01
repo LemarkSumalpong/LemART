8de6abdc3cad54603f048082d567e3e61d108642
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-02-01 18:34:12 +0800

    fixed the project link section

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -46,12 +46,12 @@ const works = [
 const Project = () => {
   return (
     <section 
-    id="work"
+    id="project"
     className="section"
     >
         <div className="container">
             <h2 className="headline-2 mb-8 ">
-                My portfolio highlights  
+                My Portfolio
             </h2>
             <div className=" grid gap-x-4 gap-y-5
              grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">

3e113ce2f6443fb900f750d0ec9f21701d915a27
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-02-02 20:49:53 +0800

    lenisreact

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@
  * @license Apache-2.0
  */
 
+import { ReactLenis, useLenis } from 'lenis/react'
+
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -13,7 +15,7 @@ import Footer from "./Footer";
 
 const App = () => {
   return (
-    <>
+   <ReactLenis root>
     <Header/>
     <main>
       <Hero/>
@@ -23,8 +25,7 @@ const App = () => {
       <Contact />
     </main>
     <Footer />
-    
-    </>
+  </ReactLenis>
   )
 }
 

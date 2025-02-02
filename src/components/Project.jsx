7e318f17528c90a832c4c817eb3b25c47b07ed4e
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-02-02 21:36:14 +0800

    progress project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,39 +6,39 @@ import ProjectCard from "./ProjectCard";
 
 const works = [
     {
-      imgSrc: '/images/project-1.jpg',
-      title: 'Full stack music app',
-      tags: ['API', 'MVC', 'Development'],
+      imgSrc: '/images/',
+      title: 'In Progress',
+      tags: ['WIP'],
       projectLink: ''
     },
     {
-      imgSrc: '/images/project-2.jpg',
-      title: 'Free stock photo app',
-      tags: ['API', 'SPA'],
+      imgSrc: '/images/',
+      title: 'In Progress',
+      tags: ['WIP'],
       projectLink: ''
     },
     {
-      imgSrc: '/images/project-3.jpg',
-      title: 'Recipe app',
-      tags: ['Development', 'API'],
+      imgSrc: '',
+      title: 'In Progress',
+      tags: ['WIP'],
       projectLink: ''
     },
     {
-      imgSrc: '/images/project-4.jpg',
-      title: 'Real state website',
-      tags: ['Web-design', 'Development'],
+      imgSrc: '/images/',
+      title: 'In Progress',
+      tags: ['WIP'],
       projectLink: ''
     },
     {
-      imgSrc: '/images/project-5.jpg',
-      title: 'eCommerce website',
-      tags: ['eCommerce', 'Development'],
+      imgSrc: '/images/',
+      title: 'In Progress',
+      tags: ['WIP'],
       projectLink: ''
     },
     {
-      imgSrc: '/images/project-6.jpg',
-      title: 'vCard Personal portfolio',
-      tags: ['Web-design', 'Development'],
+      imgSrc: '/images/',
+      title: 'In Progress',
+      tags: ['WIP'],
       projectLink: ''
     },
   ];

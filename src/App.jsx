964b94805ade21c8b8968ea7544f437dab368fd1
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-02-02 21:29:14 +0800

    commit gsap

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,10 @@
 import { ReactLenis, useLenis } from 'lenis/react'
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from '@gsap/react';
 
+gsap.registerPlugin(useGSAP, ScrollTrigger); // register the hook to avoid React version discrepancies 
 
-gsap.registerPlugin(ScrollTrigger);
 
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -19,6 +20,25 @@ import Contact from "./Contact";
 import Footer from "./Footer";
 
 const App = () => {
+  useGSAP (() => {
+    const elements = gsap.utils.toArray('.reveal-up');
+    elements.forEach((element) => {
+      gsap.to(element, {
+        scrollTrigger:{
+        trigger: element,
+        start: '-200 bottom',
+        end:'bottom 80%',
+        scrub: true
+      },
+      y: 0,
+      opacity: 1,
+      duration: 1,
+      ease: 'power2.out'
+      })
+    });
+  });
+
+
   return (
    <ReactLenis root>
     <Header/>

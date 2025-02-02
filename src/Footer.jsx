3fe3a0a037a4c082de18ef0a5d4e6329b93c05c3
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-02-02 20:33:04 +0800

    footer content

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,6 +3,8 @@
  * @license Apache-2.0
  */
 
+import { ButtonPrimary } from "./components/Button";
+
 const sitemap = [
     {
       label: 'Home',
@@ -13,12 +15,8 @@ const sitemap = [
       href: '#about'
     },
     {
-      label: 'Work',
+      label: 'Project',
       href: '#work'
-    },
-    {
-      label: 'Reviews',
-      href: '#reviews'
     },
     {
       label: 'Contact me',
@@ -54,15 +52,74 @@ const Footer = () => {
     <footer className="section">
         <div className="container">
 
-            <div className="">
-                <div className="">
-                    <h2 className="headline-1">
+            <div className="lg:grid lg:grid-cols-2">
+                <div className="mb-10">
+                    <h2 className="headline-1 mb-8 lg:max-w-[12ch]">
                          Let&apos;s work together today!
                     </h2>
+                    <ButtonPrimary
+                      href="mailto:sumalponglemark2@gmail.com"
+                      label="Connect with me"
+                      icon="chevron_right"
+                    />
                 </div>
+
+                <div className="grid grid-cols-2 gap-4 lg:pl-20">
+
+                    <div>
+                        <p className="mb-2">Sitemap</p>
+                        <ul>
+                            {sitemap.map(({label, href}, key) =>(
+                                <li key={key}>
+                                    <a 
+                                     href=""
+                                     className="block text-sm text-zinc-400 py-1
+                                     transition-colors hover:text-zinc-200"
+                                     >
+                                        {label}
+                                    </a>                                   
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+
+                    <div>
+                        <p className="mb-2">Socials</p>
+                        <ul>
+                            {socials.map(({label, href}, key) =>(
+                                <li key={key}>
+                                    <a 
+                                     href=""
+                                     target="_blank"
+                                     className="ock text-sm text-zinc-400 py-1
+                                     transition-colors hover:text-zinc-200"
+                                     >
+                                        {label}
+                                    </a>                                   
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>                
             </div>
 
+            <div className="">
+                <a 
+                 href=""
+                 className=""
+                 >
+                    <img 
+                     src="images/logo.svg" 
+                     width={40}
+                     height={40}
+                     alt="Logo"
+                    />
+                 </a>
 
+                 <p className="">
+                    &copy; 2025 <span className="">Lemark Sumalpong</span>
+                 </p>
+            </div>
         </div>    
     </footer>
   )

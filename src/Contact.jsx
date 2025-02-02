1efd29706e12d4e9755cb766b050af7644b80610
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-02-02 19:10:14 +0800

    responsive

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -46,26 +46,30 @@ const Contact = () => {
      id="contact"
      className="section"
      >
-        <div className="container lg:grid lg:grid-cols-2">
+        <div className="container lg:grid lg:grid-cols-2
+        lg:items-stretch">
 
-            <div className="">
+            <div className="mb-12 lg:mb-0 lg:flex lg:flex-col">
 
-                <h2 className="headline-2">
+                <h2 className="headline-2 lg:max-w-[12ch]:">
                     Contact me for collaboration
                 </h2>
 
-                <p className="">
+                <p className="text-zinc-400 mt-3 mb-8 max-w-[50ch]
+                lg:max-w-[30ch]">
                   Reach out today to discuss your project needs and 
                   start collaborating on something amazing!
                 </p>
 
-                <div className="">
+                <div className="flex items-center gap-2 mt-auto">
                     {socialLinks.map(({href, icon}, key) =>(
                         <a
                          key={key}
                          href={href}
                          target="_blank"
-                         className=""
+                         className="w-12 h-12 grid place-items-center ring-inset ring-2
+                          ring-zinc-50/50 rounded-lg transition[background-color,color]
+                          hover:bg-zinc-50 hover:text-zinc-950 active:bg-zinc-50/80"
                         >
                             {icon}
                         </a>
@@ -78,17 +82,37 @@ const Contact = () => {
 
         <form action="" 
          method= "POST"
-         className=""
+         className="xl:pl-10 2xl:pl-20"
          >
 
-            <div className="">
+            <div className="md:grid md:items-center md:grid-cols-2 md:gap-2">
 
-                <div className="">
+                
+            <div className="mb-4">
+                    <label 
+                     htmlFor="name"
+                     className="label"
+                    >
+                      Name: 
+                    </label>
+
+                    <input 
+                     type="name"
+                     name="name" 
+                     id="name"
+                     autoComplete="name" 
+                     required
+                     placeholder="Lemark Sumalpong"
+                     className="text-field"
+                    />
+
+                </div>
+                <div className=" mb-4">
                     <label 
                      htmlFor="email"
                      className="label"
                     >
-                       Email
+                       Email: 
                     </label>
 
                     <input 
